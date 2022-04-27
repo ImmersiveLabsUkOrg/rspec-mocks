bd59189244df8dc80b12b66eabda048ecf53c6f5
Matt Bessey <matt.bessey@immersivelabs.co.uk>
2022-04-27 16:07:04 +0100

    Re-introduce sorbet types to mocked doubles

--- lib/rspec/mocks/method_double.rb
+++ lib/rspec/mocks/method_double.rb
@@ -63,6 +63,15 @@ module RSpec
           define_method(method_name) do |*args, &block|
             method_double.proxy_method_invoked(self, *args, &block)
           end
+          key = T::Private::Methods.send(:method_owner_and_name_to_key, self, method_name)
+          sig = T::Private::Methods.send(:signature_for_key, key)
+          if sig
+            T::Private::Methods::CallValidation.rewrap_method(
+              self,
+              sig,
+              instance_method(method_name)
+            )
+          end
           # This can't be `if respond_to?(:ruby2_keywords, true)`,
           # see https://github.com/rspec/rspec-mocks/pull/1385#issuecomment-755340298
           ruby2_keywords(method_name) if Module.private_method_defined?(:ruby2_keywords)
